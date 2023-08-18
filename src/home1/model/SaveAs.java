c9fff45460820bfce7fcf116bde54a38ee60b775
KarenginaA <kroup15@mail.ru>
2023-08-18 17:14:44 +0300

    Initial commit

---
+++ src/home1/model/SaveAs.java
@@ -0,0 +1,8 @@
+package home1.model;
+
+import java.io.Serializable;
+
+public interface SaveAs {
+    void save(String path, Serializable obj);
+}
+
---
+++ src/home1/ui/commands/Load.java
@@ -0,0 +1,20 @@
+package home1.ui.commands;
+
+import home1.ui.Console;
+
+public class Load extends Command {
+    public Load(Console console) {
+        super(console);
+    }
+
+    @Override
+    public String description() {
+        return "Загрузить семью. ";
+    }
+
+    @Override
+    public void execute() {
+        getConsole().loadFamily();
+    }
+}
+
